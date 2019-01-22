f3585120aa352810dc41018943b25b820bd107db
arpit agrawal <arpitagr.2011@gmail.com>
2019-01-23 01:21:44 +0530

    change onClicklistener from onbind to onCreate

--- app/src/main/java/com/example/arpit/musicapp/MyAdapter.java
+++ app/src/main/java/com/example/arpit/musicapp/MyAdapter.java
@@ -28,16 +28,50 @@ public class MyAdapter extends RecyclerView.Adapter {
         }
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView mTextView;
         public ImageButton mPlay;
         public  MyViewHolder(View v){
             super(v);
             this.mTextView = v.findViewById(R.id.title);
             this.mPlay = (ImageButton) v.findViewById(R.id.play_pause);
+            this.mPlay.setOnClickListener(this);
            // ((View)mPlay).setOnClickListener(MyAdapter.this);
         }
-    }
+
+        @Override
+            public void onClick(View v) {
+
+                if(stop==true){
+                    stop = false;
+
+                    //ViewGroup.LayoutParams params_playbar = playbar.getLayoutParams();
+                    //ViewGroup.LayoutParams params_rv = rv.getLayoutParams();
+                    //params_playbar.height = rv.getHeight()/10;
+                    //playbar.requestLayout();
+                    //ImageButton pp = playbar.findViewById(R.id.bar_album);
+                    //rv.requestLayout();
+                    /// ImageButton bar_pp = playbar.findViewById(R.id.bar_pp);
+                    //bar_pp.setBackgroundResource(R.drawable.play);
+                    playbar.setVisibility(View.VISIBLE);
+                }
+
+                int position = this.getAdapterPosition();   //position of this view holder which is clicked
+                ImageButton bt = (ImageButton)v;
+                if(position==playing){
+                    playing = -1;
+                    bt.setBackgroundResource(R.drawable.pause);
+                }
+                else{
+                    playing = position;
+                    if(mPlaying != null)
+                        MyAdapter.mPlaying.setBackgroundResource(R.drawable.pause);
+                    mPlaying = bt;
+                    bt.setBackgroundResource(R.drawable.play);
+                }
+            }
+
+        }
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
 
@@ -51,38 +85,7 @@ public class MyAdapter extends RecyclerView.Adapter {
 
         vh.mTextView.setText(arr[position]);
        // final View view = this.playbar;
-        vh.mPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                if(stop==true){
-                    stop = false;
-
-                    //ViewGroup.LayoutParams params_playbar = playbar.getLayoutParams();
-                    //ViewGroup.LayoutParams params_rv = rv.getLayoutParams();
-                    //params_playbar.height = rv.getHeight()/10;
-                    //playbar.requestLayout();
-                    //ImageButton pp = playbar.findViewById(R.id.bar_album);
-                    //rv.requestLayout();
-                   /// ImageButton bar_pp = playbar.findViewById(R.id.bar_pp);
-                    //bar_pp.setBackgroundResource(R.drawable.play);
-                      playbar.setVisibility(View.VISIBLE);
-                }
-
-                ImageButton bt = (ImageButton)v;
-                if(position==playing){
-                        playing = -1;
-                        bt.setBackgroundResource(R.drawable.pause);
-                    }
-                    else{
-                    playing = position;
-                        if(mPlaying != null)
-                            MyAdapter.mPlaying.setBackgroundResource(R.drawable.pause);
-                        mPlaying = bt;
-                        bt.setBackgroundResource(R.drawable.play);
-                    }
-            }
-        });
 
 
     }
@@ -93,6 +96,11 @@ public class MyAdapter extends RecyclerView.Adapter {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.songrow, parent, false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
+
+
+
+
+
     }
 
     @Override
